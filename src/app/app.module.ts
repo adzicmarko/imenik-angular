bac4c0096fc422e721b18dd6b4219039ed57ca33
adzicmarko <coding.working@gmail.com>
2020-02-15 10:54:58 +0100

    bootstrap, jquery, servis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistracijaComponent } from './registracija/registracija.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistracijaComponent
   ],
   imports: [
     BrowserModule,
