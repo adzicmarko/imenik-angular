2690f6acaebbe12ee8cb78189385453ae96b38e0
adzicmarko <coding.working@gmail.com>
2020-02-16 11:32:23 +0100

    kompletirana pocetna strana

--- src/app/pocetna/pocetna.component.ts
+++ src/app/pocetna/pocetna.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { ServisService } from '../servis.service';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pocetna',
@@ -19,25 +19,21 @@ export class PocetnaComponent implements OnInit {
   vremeUpisa;
   izmenjenoIme = "";
   izmenjenTelefon = "";
+  @ViewChild('closebutton', { static: false }) closebutton;
 
   constructor(private http: HttpClient, private toastr: ToastrService, private servis: ServisService) { }
 
   ngOnInit(): void {
     this.servis.proveriToken();
-    setInterval(() => {this.servis.proveriToken();}, 50000);
-    
+    setInterval(() => { this.servis.proveriToken(); }, 50000);
+
     this.formdata = new FormGroup({
       imeIprezime: new FormControl("", this.imeIprezimeValidacija),
       telefon: new FormControl("", this.telefonValidacija),
     });
-    
-    this.formatdataModal = new FormGroup({
-      imeIprezimeM: new FormControl(this.imeIprezimeValidacijaM, this.imeIprezimeValidacijaM),
-      telefonM: new FormControl(this.telefonValidacijaM, this.telefonValidacijaM),
-    });
 
-    
-    this.izlistajKontakte ();
+
+    this.izlistajKontakte();
   }
 
 
@@ -51,52 +47,42 @@ export class PocetnaComponent implements OnInit {
       return { "telefon": true }
     }
   }
-  imeIprezimeValidacijaM(polje) {
-    if (polje.value.length < 1) {      
-      this.izmenjenoIme = polje;
-      return { "imeIprezimeM": true }
-    }
-  }
-  telefonValidacijaM(polje) {
-    if (polje.value.length < 1) {
-      this.izmenjenTelefon = polje;
-      return { "telefonM": true }
-    }
-  }
 
-  dodajKontakt(podaci){
+
+  dodajKontakt(podaci) {
     let kontakt = {
       imeiprezime: podaci.imeIprezime,
       telefon: podaci.telefon
     };
     let jsonKontakt = JSON.stringify(kontakt);
-    this.upisiKontakt(jsonKontakt).subscribe((data)=>{
-      if (Object(data).sifra == 1){
+    this.upisiKontakt(jsonKontakt).subscribe((data) => {
+      if (Object(data).sifra == 1) {
         this.toastr.success('Info', Object(data).poruka);
       } else {
-        this.toastr.error('Grešska', Object(data).poruka);
+        this.toastr.error('Greška', Object(data).poruka);
       }
     });
+    this.izlistajKontakte();
   }
 
-  izlistajKontakte (){
-    this.ucitajKontakte().subscribe((data)=>{
-      for (let i =0; i< Object(data).length; i++){
+  izlistajKontakte() {
+    this.kontakti = [];
+    this.ucitajKontakte().subscribe((data) => {
+      for (let i = 0; i < Object(data).length; i++) {
         let kontakt = {
           imeiprezime: Object(data)[i].imeiprezime,
           telefon: Object(data)[i].telefon,
           id: Object(data)[i].id,
           vreme: Object(data)[i].vreme_upisa,
-          korisnik_id: Object(data)[i].korisnik_id,          
-          }
-          this.kontakti.push(kontakt);
+          korisnik_id: Object(data)[i].korisnik_id,
+        }
+        this.kontakti.push(kontakt);
       }
     });
   }
 
-
-  prikaziKontakt(id){
-    this.ucitajKontakt(id).subscribe((data)=>{
+  prikaziKontakt(id) {
+    this.ucitajKontakt(id).subscribe((data) => {
       this.kontaktImeIprezime = Object(data).imeiprezime;
       this.kontaktTelefon = Object(data).telefon;
       this.idKontakta = id;
@@ -104,26 +90,46 @@ export class PocetnaComponent implements OnInit {
     });
   }
 
+  izmeniKontakt(id, izmenjenoIme, izmenjenTelefon) {
+    for (let i = 0; i < this.kontakti.length; i++) {
+      if (this.kontakti[i].id == id) {
+        if(izmenjenoIme.length < 1 || izmenjenTelefon.length < 1){
+          this.toastr.error('Info', "Popunite sva polja");
+        } else {
+        this.kontakti[i].imeiprezime = izmenjenoIme;
+        this.kontakti[i].telefon = izmenjenTelefon;
+        let jsonIzmenjenKontakt = JSON.stringify(this.kontakti[i]);
+        this.izmenaKontakta(id, jsonIzmenjenKontakt).subscribe((data) => {
+          this.toastr.success('Info', Object(data).poruka);
+          this.closebutton.nativeElement.click();
+        });
+      }
+      }
+    }
+  }
 
-  izmeniKontakt(id){
-    console.log(id, this.izmenjenoIme);
-    // let izmenjenKontakt = {
-    //   imeiprezime: id.imeIprezime,
-    //   telefon: id.telefon
-    // };
-    // console.log(izmenjenKontakt.imeiprezime);
+  obrisiKontakt(id){
+    this.brisanjeKontakta(id).subscribe((data)=>{
+      this.toastr.success('Info', Object(data).poruka);
+      this.izlistajKontakte();
+    }); 
+  }
 
-    // for (let i = 0; i< this.kontakti.length; i++){
-    //   if (this.kontakti[i].idKontakta == id){
-    //     this.kontakti[i].imeiprezime = izmenjenKontakt.imeiprezime;
-    //     this.kontakti[i].telefon = izmenjenKontakt.telefon;
-    //   }
-    // }
-    this.izlistajKontakte ();
-
-
-
-
+  upisiKontakt(kontakt) {
+    return this.http.post(this.servis.apiUrl + "dodajKontakt/" + this.servis.token, kontakt);
+  }
+  ucitajKontakte() {
+    return this.http.get(this.servis.apiUrl + "ucitajKontakte/" + this.servis.token);
+  }
+  ucitajKontakt(id) {
+    return this.http.get(this.servis.apiUrl + "kontaktInfo/" + this.servis.token + "/" + id);
+  }
+  izmenaKontakta(id, kontakt) {
+    return this.http.post(this.servis.apiUrl + "izmeniKontakt/" + this.servis.token + "/" + id, kontakt);
+  }
+  brisanjeKontakta(id) {
+    return this.http.get(this.servis.apiUrl + "obrisiKontakt/" + this.servis.token + "/" + id);
+  }
 
 
 
@@ -133,27 +139,3 @@ export class PocetnaComponent implements OnInit {
 
 
   }
-
-  upisiKontakt(kontakt){
-    return this.http.post(this.servis.apiUrl+"dodajKontakt/"+this.servis.token, kontakt);     
-  }
-
-  ucitajKontakte(){
-    return this.http.get(this.servis.apiUrl+"ucitajKontakte/"+this.servis.token);     
-  }
-
-  ucitajKontakt(id){
-    return this.http.get(this.servis.apiUrl+"kontaktInfo/"+this.servis.token+"/"+id);     
-  }
-  
-
-
-  // $.get("http://obrada.in.rs/api/kontaktInfo/" + token + "/" + id, function (data) 
-    // $.post("http://obrada.in.rs/api/izmeniKontakt/"+ token + "/" + id, jsonIzmenjenKontakt, function (data) 
-  // $.get("http://obrada.in.rs/api/obrisiKontakt/" + token + "/" + id, function (data)
-
-
-
-
-
-}
